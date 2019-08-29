36d02bf858da2362f3b31f738d26090081452065
alisha musamabik <alisha.musamabik@gmail.com>
2019-08-29 20:04:07 +0100

    Seed for the house,area,user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,55 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Deleteing all data'
+Area.destroy_all
+House.destroy_all
+User.destroy_all
+puts 'Data deleted'
+puts 'Creating Users, Areas and Houses'
+# images here
+
+5.times do
+  area = Area.new(
+    name:Faker::HarryPotter.character,
+    sold_price: rand(500000..1000000),
+    schools:Faker::HarryPotter,
+    crime:rand(),
+    demographics:rand() ,
+    price:rand(500000..1000000),
+    growth:rand(),
+    demand: rand()
+  )
+  area.save!
+end
+
+5.times do
+  user = User.new(
+    email: Faker::Internet.email,
+    username: Faker::Internet.username,
+    name: Faker::Name.name,
+    password: '123456'
+  )
+  user.save!
+  counter = 0
+end
+
+5.times do
+  house = House.new(
+    address: Faker::Address.street_address,
+    postcode: Faker::Address.postcode,
+    bedroom: rand(1..6),
+    description: Faker::Lorem.paragraph(sentence_count: 2),
+    rate: rand(150..500),
+    bathroom: rand(10..40),
+    square_feet:rand(100..300),
+    council_tax: rand(a,b,c,d,e,f),
+    ptal:rand( 1a, 1b, 2, 3, 4, 5, 6a, 6b),
+    green_belt: rand( true, false),
+    area: Area.all.sample,
+    restaurants:Faker::Restaurant.name
+  )
+end
+house.save!
+end
+puts 'Finished!'
