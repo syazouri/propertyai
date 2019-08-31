46f498d26def1c702f85c0c7fe35d78e5d7025a4
Sarah Yazouri <syazouri@gmail.com>
2019-08-31 04:34:32 -0700

    changes to profile page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_192006) do
+ActiveRecord::Schema.define(version: 2019_08_31_111643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,13 @@ ActiveRecord::Schema.define(version: 2019_08_29_192006) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.integer "income"
+    t.integer "credit_score"
+    t.datetime "date_of_birth"
+    t.integer "deposit"
+    t.integer "borrowing"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
