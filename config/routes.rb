d9189731894eeed3342081e47ced7451c515e6cb
Emmanuel Narkuwah <coding@bigballer.co.uk>
2019-09-03 13:00:40 +0100

    adding first changes to areas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :areas, only: [:index, :show] do
+  resources :areas, only: [:index, :show, :edit, :update] do
     resources :houses, only: [:index, :show]
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
