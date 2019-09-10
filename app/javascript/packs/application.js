4a07a105f25eed671b61b297e20bad2480e0529c
Emmanuel Narkuwah <coding@bigballer.co.uk>
2019-09-10 15:47:44 +0100

    added popover to show area

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import "../plugins/area_show";
 
 
 function multiplyBy()
@@ -11,4 +12,5 @@ function multiplyBy()
 document.getElementById("multiply").addEventListener('click', () => {
   multiplyBy();
 })
+
 
--- app/javascript/plugins/popover.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  $('[data-toggle="popover"]').popover();
-});
-
-const popOver = () => {
-
-}
