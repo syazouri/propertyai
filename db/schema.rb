132d3df218aa025d3077b199c3cbb14ba2324054
alisha musamabik <alisha.musamabik@gmail.com>
2019-09-14 16:39:43 +0100

    update search view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_09_14_122404) do
 
   # These are extensions that must be enabled in order to support this database
@@ -67,7 +66,6 @@ ActiveRecord::Schema.define(version: 2019_09_14_122404) do
     t.integer "credit_score"
     t.boolean "school"
     t.integer "distance_to_work"
-    t.bigint "area_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -76,7 +74,6 @@ ActiveRecord::Schema.define(version: 2019_09_14_122404) do
     t.string "work_postcode"
     t.string "price"
     t.string "crime"
-    t.index ["area_id"], name: "index_searches_on_area_id"
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
@@ -103,6 +100,5 @@ ActiveRecord::Schema.define(version: 2019_09_14_122404) do
 
   add_foreign_key "houses", "areas"
   add_foreign_key "prices", "areas"
-  add_foreign_key "searches", "areas"
   add_foreign_key "searches", "users"
 end
