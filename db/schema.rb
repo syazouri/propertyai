75e55414376a4f2b9940ee96b69309fb048c3b4c
Emmanuel Narkuwah <coding@bigballer.co.uk>
2019-09-17 20:12:07 +0100

    schools geocode

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_122404) do
+ActiveRecord::Schema.define(version: 2019_09_14_150700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2019_09_14_122404) do
     t.integer "credit_score"
     t.boolean "school"
     t.integer "distance_to_work"
+    t.bigint "area_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -76,6 +77,7 @@ ActiveRecord::Schema.define(version: 2019_09_14_122404) do
     t.string "work_postcode"
     t.string "price"
     t.string "crime"
+    t.index ["area_id"], name: "index_searches_on_area_id"
     t.index ["user_id"], name: "index_searches_on_user_id"
   end
 
@@ -102,5 +104,6 @@ ActiveRecord::Schema.define(version: 2019_09_14_122404) do
 
   add_foreign_key "houses", "areas"
   add_foreign_key "prices", "areas"
+  add_foreign_key "searches", "areas"
   add_foreign_key "searches", "users"
 end
