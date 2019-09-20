1bdb1dbae7415fa37e5f84aaa14b56185ce415c1
Emmanuel Narkuwah <coding@bigballer.co.uk>
2019-09-20 17:48:36 +0100

    property table added

--- app/javascript/plugins/area_school.js
+++ app/javascript/plugins/area_school.js
@@ -1,6 +1,7 @@
-document.addEventListener("click", (event) => {
-  const schoolInfo = document.querySelector(".school-info");
-  const schoolAreaInfo = document.querySelector(".area-school-info");
-  schoolAreaInfo.innerHTML =  schoolInfo.innerHTML;
-});
+const schoolMap = document.querySelector("#map");
+  schoolMap.addEventListener("click", (event) => {
+    const schoolInfo = document.querySelector(".school-info");
+    const schoolAreaInfo = document.querySelector(".area-school-info");
+    schoolAreaInfo.innerHTML =  schoolInfo.innerHTML;
+  });
 
--- app/javascript/plugins/area_show.js
+++ app/javascript/plugins/area_show.js
@@ -1,8 +1,13 @@
-
-
-const areaShowList = document.querySelector(".area-show-list");
-if (areaShowList) {
+const areaPopOver = document.querySelector(".area-popover");
+if (areaPopOver) {
   $(document).ready(function(){
     $('[data-toggle="popover"]').popover();
   });
 }
+
+const areaToolTip = document.querySelector(".area-tooltip");
+// if (areaToolTip) {
+  $(document).ready(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+  });
+// }
