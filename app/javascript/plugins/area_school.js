4eda4bc84b0a1685e9c80867afb136af1b5b5413
alisha musamabik <alisha.musamabik@gmail.com>
2019-09-21 16:54:34 +0100

    chatbox front part3

--- app/javascript/plugins/area_school.js
+++ app/javascript/plugins/area_school.js
@@ -1,7 +1,9 @@
 const schoolMap = document.querySelector("#map");
+
+if (schoolMap) {
   schoolMap.addEventListener("click", (event) => {
     const schoolInfo = document.querySelector(".school-info");
     const schoolAreaInfo = document.querySelector(".area-school-info");
     schoolAreaInfo.innerHTML =  schoolInfo.innerHTML;
   });
-
+}
