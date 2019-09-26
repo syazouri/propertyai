eba787f230721cecdacd8f7ce34bcaa2acbca52b
Sarah Yazouri <syazouri@gmail.com>
2019-09-26 12:56:56 -0700

    lots of edits

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -30,10 +30,10 @@ function calculateMortgage() {
       if (!isNaN(num1)) {
         const result1 = num1 * 3.25;
         const result2 = num1 * 4.5;
-        const btn = document.getElementById("multiply");
+        const btn = document.getElementById("after");
         const html = `<p><br>We estimate you can borrow £<span id = "result1">${result1}</span> - £<span id = "result2">${result2}</span><br>
         </p><div class="form-inputs"><input name="search[deposit]" type="text" placeholder="Enter your deposit amount" id="deposit-amount"></div>`;
-        btn.insertAdjacentHTML("afterend", html);
+        btn.insertAdjacentHTML("afterbegin", html);
         const maxMortgage = document.getElementById("max-mortgage");
         maxMortgage.value = result2;
       }
